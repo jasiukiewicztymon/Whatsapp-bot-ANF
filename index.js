914f2a1d3555953a6c320e09a169c073c178a5c2
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-08 17:33:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 const puppeteer = require('puppeteer');
 const data = require('./data.json');
 const fs = require('fs');
+var ks = require('node-key-sender');
+
+ks.setOption('caseCorrection', false);
+ks.setOption('globalDelayPressMillisec', 1);
+ks.setOption('globalDelayBetweenMillisec', 1);
+ks.setOption('startDelayMillisec', 1);
+
+var accentsMap = {
+    'ã': '@514 a',
+    'ẽ': '@514 e',
+    'ĩ': '@514 i',
+    'õ': '@514 o',
+    'ũ': '@514 u',
+    'Ã': '@514 A',
+    'Ẽ': '@514 E',
+    'Ĩ': '@514 I',
+    'Õ': '@514 O',
+    'Ũ': '@514 U',
+    'â': 'shift-@514 a',
+    'ê': 'shift-@514 e',
+    'î': 'shift-@514 i',
+    'ô': 'shift-@514 o',
+    'û': 'shift-@514 u',
+    'Â': 'shift-@514 A',
+    'Ê': 'shift-@514 E',
+    'Î': 'shift-@514 I',
+    'Ô': 'shift-@514 O',
+    'Û': 'shift-@514 U',
+    'à': 'shift-@192 a',
+    'è': 'shift-@192 e',
+    'ì': 'shift-@192 i',
+    'ò': 'shift-@192 o',
+    'ù': 'shift-@192 u',
+    'À': 'shift-@192 A',
+    'È': 'shift-@192 E',
+    'Ì': 'shift-@192 I',
+    'Ò': 'shift-@192 O',
+    'Ù': 'shift-@192 U',
+    'á': '@192 a',
+    'é': '@192 e',
+    'í': '@192 i',
+    'ó': '@192 o',
+    'ú': '@192 u',
+    'Á': '@192 A',
+    'É': '@192 E',
+    'Í': '@192 I',
+    'Ó': '@192 O',
+    'Ú': '@192 U',
+    'ç': '@192 c',
+    'Ç': '@192 C',
+    'ä': 'shift-@54 a',
+    'ë': 'shift-@54 e',
+    'ï': 'shift-@54 i',
+    'ö': 'shift-@54 o',
+    'ü': 'shift-@54 u',
+    'Ä': 'shift-@54 A',
+    'Ë': 'shift-@54 E',
+    'Ï': 'shift-@54 I',
+    'Ö': 'shift-@54 O',
+    'Ü': 'shift-@54 O'
+};
+
+ks.aggregateKeyboardLayout(accentsMap);
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -8,11 +71,6 @@ function sleep(milliseconds) {
     do {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
-}
-
-async function type(e) {
-    await page.evaluate(() => { document.querySelector('._2vbn4').click(); });
-    await page.$eval('._2vbn4', el => el.value = (date + ", " + data.name + " dispo " + e).toString());
 }
 
 // datas
@@ -40,11 +98,15 @@ var date = "", convocation = "";
             if (!data.date.includes(date)) {
                 data.place.forEach(e => {
                     if (convocation.toLowerCase().includes(e)) {
+                        (async function type() {
+                            // 2. Writing text
+                            ks.sendText((e).toString());
+                        })();
                         console.log(e)
                     }
                 });
             }
-            sleep(100)
+            sleep(1000)
         }        
     }
     catch (err) {
