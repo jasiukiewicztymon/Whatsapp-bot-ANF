905635d6ceac150d8ac4910c7ffbfc5d251b7e31
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-05-08 17:51:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,25 +19,29 @@ var date = "", convocation = "", res = "", ok = false;
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3641.0 Safari/537.36');
         await page.goto('https://web.whatsapp.com');
-        await page.waitForSelector("._25pwu");
-        await page.waitForSelector(".ldL67");
+        await page.waitForSelector("._1AHcd");
         // connected to Whatsapp...
-        page.click('span[title="Test"]');
+        page.click('span[title="Groupe Arbitres Juniors D"]');
         // go to the Junior chat
+
+        sleep(5000);
+
         while (1) {
             try {
                 await page.waitForSelector('strong');
                 const text = await page.evaluate(() => Array.from(document.querySelectorAll('strong')[document.querySelectorAll('strong').length - 1].parentElement.querySelectorAll('strong'), element => element.textContent));
+
                 while (text.length != 0) {
                     convocation = text[text.length - 1];
                     text.pop()
-                    date = convocation.substr((convocation.indexOf('.') - 2), 5)
+                    date = convocation.substring((convocation.indexOf('.') - 2), (convocation.indexOf('.') - 2) + 5)
+
                     if (!data.date.includes(date)) {
                         for (var i = 0; i < data.place.length; i++) {
                             e = data.place[i];
                             if (convocation.toLowerCase().includes(e)) {
                                 (async function type() {
-                                    res = date + " " + e + ", " + data.name + " dispo" 
+                                    res = date + " " + e + ", ";
 
                                     for (var j = 0; j < res.length; j++) {
                                         page.keyboard.press(res[j]);
@@ -59,7 +63,13 @@ var date = "", convocation = "", res = "", ok = false;
                     }
                 }
                 if (ok) {
-                    page.click("span[data-testid='send']")
+                    res = data.name + " dispo";
+                    for (var j = 0; j < res.length; j++) {
+                        page.keyboard.press(res[j]);
+                    }
+
+                    await page.waitForSelector('#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._1VZX7 > div._2xy_p._3XKXx > button > span');
+                    page.click("#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._1VZX7 > div._2xy_p._3XKXx > button > span")
                     ok = false;
                 }
             }
